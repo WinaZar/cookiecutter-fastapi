bdd202488fc6467af0d2fd75122e8afd61539978
Stepan Nazarov <stepanartov@gmail.com>
2021-01-09 23:35:55 +0300

    Add all_users query

--- backend/schema.py
+++ backend/schema.py
@@ -1,13 +1,36 @@
-from typing import Any
+from typing import List
 
 import graphene
+from fastapi import Request
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from graphql.execution.base import ResolveInfo
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from backend.db.models import User
+
+
+async def _get_engine(info: ResolveInfo) -> AsyncEngine:
+    request: Request = info.context["request"]
+    return request.app.state.engine
+
+
+class UserSchema(SQLAlchemyObjectType):
+    class Meta:
+        model = User
+        exclude_fields = ["password"]
 
 
 class RootQuery(graphene.ObjectType):
-    hello = graphene.String(name=graphene.String(default_value="stranger"))
+    all_users = graphene.List(UserSchema)
 
-    async def resolve_hello(self, info: Any, name: str) -> str:
-        return "Hello " + name
+    async def resolve_all_users(self, info: ResolveInfo) -> List[User]:
+        engine = await _get_engine(info)
+        async with AsyncSession(engine) as session:
+            query_result = await session.execute(select(User))
+        users: List[User] = query_result.scalars().fetchall()
+        return users
 
 
 schema = graphene.Schema(query=RootQuery, auto_camelcase=False)
